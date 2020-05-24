19844f64e6888cefa3fbaa82196f864c255239b9
SkNuwanTissera <nuwan.tissera@my.sliit.lk>
2020-05-24 20:21:59 +0530

    reverse

--- src/Solution.java
+++ src/Solution.java
@@ -10,21 +10,25 @@ public class Solution {
 
     // Complete the reverseArray function below.
     static int[] reverseArray(int[] a) {
-        return null;
+       int[] newarr = new int[a.length];
+       int count = 0;
+        for (int x=a.length-1; x>=0 ; x--) {
+            newarr[count] = a[x];
+            count++;
+        }
+        return newarr;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int arrCount = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int arrCount = Integer.parseInt(scanner.nextLine());
+        String[] arrItems = scanner.nextLine().split(" ");
 
         int[] arr = new int[arrCount];
-
-        String[] arrItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+//        System.out.println(arr.length);
+        scanner.skip("");
 
         for (int i = 0; i < arrCount; i++) {
             int arrItem = Integer.parseInt(arrItems[i]);
@@ -33,18 +37,15 @@ public class Solution {
 
         int[] res = reverseArray(arr);
 
-        for (int i = 0; i < res.length; i++) {
-            bufferedWriter.write(String.valueOf(res[i]));
-
-            if (i != res.length - 1) {
-                bufferedWriter.write(" ");
-            }
-        }
-
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
+        printArray(res);
         scanner.close();
+    }
+
+    private static void printArray(int[] res) {
+        String output = null;
+        for (int i = 0; i < res.length; i++) {
+            output.concat(" "+res[i]);
+        }
+        System.out.println(output);
     }
 }
